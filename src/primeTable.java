0bcc2e1b824dacfaab8855f1db5819e96d378e8d
Nickochan <nickochan804@gmail.com>
2021-03-26 15:49:21 -0500

    lab 1 prime table update 3 26

--- src/primeTable.java
+++ src/primeTable.java
@@ -4,16 +4,17 @@ public class primeTable
 
 	public static void main(String args[]) 
 	{
-		int low = 14;
-		int high = 38;
+		int low = 4;
+		int high = 15;
 		
 		primeTable(low, high);
 		
 		
 	}
 	
-	static void primeTable(int low, int high) 
+	static void getPrimes(int low, int high) 
 	{
+		System.out.print("        ");
 		int i;		//number to check if it is prime
 		int j;		//divisor increasing from 2 to i
 		int flag;	// flag variable to check if i is prime
@@ -34,9 +35,28 @@ public class primeTable
 				System.out.print(form(i));
 		}
 		
-		
+		System.out.println();
+		// second nested loop to do the multiplication
+		for(i = low; i<=high; i++)
+		{
+			flag = 0;
+			for(j = 2; j < i; j++)
+			{
+				if(i%j ==0) 
+				{
+					flag = 1;
+					break;
+				}
+					
+			}
+			if (flag == 0)
+				System.out.print(i+":      ");
+			
+		}
 		
 	}
+	
+	static void primeTable()
 	
 	static String form(int i)
 	{
