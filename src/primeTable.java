4eb41c7ac6b3abfb098cb58d95b54bb95d86a372
Nickochan804 <nickochan804@gmail.com>
2021-03-28 17:46:22 -0500

    prime tables completed

--- src/primeTable.java
+++ src/primeTable.java
@@ -1,13 +1,13 @@
-
+// There is probably an easier what to do this but I was struggling to figure it out. I know I have a lot of repeating code but it works.
 public class primeTable 
 {
 
 	public static void main(String args[]) 
 	{
 		int low = 4;
-		int high = 15;
+		int high = 20;
 		
-		primeTable(low, high);
+		getPrimes(low, high);
 		
 		
 	}
@@ -19,7 +19,7 @@ public class primeTable
 		int j;		//divisor increasing from 2 to i
 		int flag;	// flag variable to check if i is prime
 		
-		for(i = low; i<=high; i++)
+		for(i = low; i<=high; i++)  //first loop to print out prime row at top of table
 		{
 			flag = 0;
 			for(j = 2; j < i; j++)
@@ -37,7 +37,7 @@ public class primeTable
 		
 		System.out.println();
 		// second nested loop to do the multiplication
-		for(i = low; i<=high; i++)
+		for(i = low; i<=high; i++)  
 		{
 			flag = 0;
 			for(j = 2; j < i; j++)
@@ -49,14 +49,31 @@ public class primeTable
 				}
 					
 			}
-			if (flag == 0)
-				System.out.print(i+":      ");
-			
+			if (flag == 0) {
+				System.out.print(form(i));
+				int col = i;
+				for(int x = low; x<=high; x++)  
+				{
+					flag = 0;
+					for(int y = 2; y < x; y++)
+					{
+						if(x%y ==0) 
+						{
+							flag = 1;
+							break;
+						}
+							
+					}
+					if (flag == 0)
+						System.out.print(form(x*col));
+			}
 		}
+		
+		System.out.println();}
 		
 	}
 	
-	static void primeTable()
+	//static void primeTable()
 	
 	static String form(int i)
 	{
